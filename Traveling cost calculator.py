8aa6bbca581381517420c4be603cfc010f7f4200
Somnath Malik <114238887+Somnath4@users.noreply.github.com>
2022-12-11 09:49:26 +0530

    Update Traveling cost calculator.py

--- Traveling cost calculator.py
+++ Traveling cost calculator.py
@@ -6,12 +6,12 @@ try:
     car_rent = int(input('Enter weekly car rent cost($): '))    # Enter weekly car rent amount
     duration = int(input('Enter duration(days): '))             # Enter how many days you're gonna stay
     car_rent_day = car_rent / duration                          # Weekly car cost divided by total duration day 
-    city = input('Enter the city name: ')                           # Enter the city name you want to visit
+    city = input('Enter the city name: ')                       # Enter the city name you want to visit
     def cost_of_trip():
         cost = return_flight + (hotel + car_rent_day + food) * duration    # Calculation of traveling cost
         return(cost)
     total_cost = cost_of_trip()
-    print('{} days trip to {} cost ${}'.format(duration, city, total_cost))  # Returns the total cost including days and the city
+    print('{} days trip to {} will cost ${}'.format(duration, city, total_cost))  # Returns the total cost including days and the city
 
 except:
     print('You can only enter numbers except in city')      # Error message when enter wrong
